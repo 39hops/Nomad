cb2ae4a085a885d330246aa04c7f1f1acfb30d16
Mohamed Dualeh <mdualeh16@gmail.com>
2024-07-29 00:33:19 -0400

    Updated href for searchresults, new login.php file Nomad home is clickable from searchresults. Fixed titles of pages to be consistant.

---
+++ develop/php/login.php
@@ -0,0 +1,5 @@
+<?php
+
+include ("db_connection.php");
+
+
--- develop/php/search-results.php
+++ develop/php/search-results.php
@@ -7,7 +7,7 @@ $searchResults = $_SESSION["search-results"];
 $json_output = json_encode($searchResults);
 
 echo " <div class='nav'>
-<span id='nomad'>NOMAD</span>
+<a href='../php/index.php'><span id='nomad'>NOMAD</span></a>
 <a href='../pages/login.html'>LOGIN</a>
 <a href='../pages/signup.html'>SIGNUP</a>
 </div> ";
@@ -49,7 +49,7 @@ echo " <div class='nav'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/search-results.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>NOMAD | Search</title>
 </head>
 
 <body>
