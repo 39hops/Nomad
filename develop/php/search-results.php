8f7e83a9d36846881a30387bdb998b502174295b
a <80867545+39hops@users.noreply.github.com>
2024-07-30 16:04:28 -0400

    Update search-results.php to have correct href for login.php

--- develop/php/search-results.php
+++ develop/php/search-results.php
@@ -36,7 +36,7 @@ if ($result->num_rows > 0) {
     <div class='search-results-container'>
         <div class='nav'>
             <a href='../php/index.php'><span id='nomad'>NOMAD</span></a>
-            <a href='../pages/login.html'>LOGIN</a>
+            <a href='../php/login.php'>LOGIN</a>
             <a href='../pages/signup.html'>SIGNUP</a>
         </div>
 
