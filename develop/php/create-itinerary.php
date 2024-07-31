39719b04d6f6efc4967e34ce71872102a14993ae
rahm0143 <ishaarahman@gmail.com>
2024-07-30 23:00:32 -0400

    create itinerary patch
    edited php to fix bug after check login credentials were changed

--- develop/php/create-itinerary.php
+++ develop/php/create-itinerary.php
@@ -18,7 +18,7 @@ if (!isset($_POST["it_name"]) || empty($_POST["it_name"])) {
 }
 
 $it_name = $_POST["it_name"];
-$user_id = $_SESSION["userid"];
+$user_id = $_SESSION["user"][0]->id;
 
 if (!$conn_stmt->bind_param("si", $it_name, $user_id)) {
     die("SQL Error (bind_param): " . $conn_stmt->error);
