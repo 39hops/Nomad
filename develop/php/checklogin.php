d486be61bfa4233109a6ab5b100e130787c98538
a <80867545+39hops@users.noreply.github.com>
2024-08-11 13:09:53 -0400

    Revert "Comments for checklogin"
    This reverts commit 7156836f6386f7aa5b54bd3f94fa1285f342ee10.

--- develop/php/checklogin.php
+++ develop/php/checklogin.php
@@ -1,40 +1,19 @@
-<!--Names: Artin Azizi (041131883), Mohamed Dualeh (41137299), Raisa Rahman (041129634)
-    Professor: Alemeseged Legesse
-    File Name: checklogin.php
-    Date: 8/11/2024
-    Description: PHP file to check to see if a user's login information is correct. 
--->
 <?php 
-/**
- * Starting session to log user information if their login is correct.
- */
 session_start();
-/**
- * Including database connection script.
- */
-include "db_connection.php";
-/**
- * Checking to see if username and password were set in the login form.
- */
+include ("db_connection.php");
+
+
+
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $uname =  $_POST['username'];
     $pass = $_POST['password']; 
-    /**
-     * Returning error message if username is empty.
-     */
     if (empty($uname)) {
         header("Location: ../pages/login.php?error=Please enter username or password. ");
         exit();
-    /**
-     * Returning error message if password is empty.
-     */
     } else if (empty($pass)) {
         header("Location: ../pages/login.php?error=Please enter username or password. ");
         exit();
     } else {
-        /**
-         * If username and password are not empty, query the database to find the correct user.
-         */
         try {
             $sql = "SELECT *
                 FROM user
@@ -44,36 +23,23 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                 $row = $result->fetch_assoc();
                 var_dump($row);
                 if (password_verify($pass, $row["u_password"])){
-                    /**
-                     * If user information is correct, log all user information.
-                     */
+
                     $userObj[] = (object) $row;
                     $_SESSION['user'] = $userObj;
                     $_SESSION['loggedIn'] = true;
-                    /**
-                     * Redirect to index if user is now logged in.
-                     */
+                    
                     header("Location: ../pages/index.php");
                     exit();
                 } 
             } else {
-                /**
-                 * If file parsed to here, their information was not correct, so redirect to login page with correct error message.
-                 */
                 header("Location: ../pages/login.php?error=Incorrect username or password. ");
                 exit();
             }   
-        /**
-         * Catching exception.
-         */
         } catch (PDOException $e) {
             die("DB ERROR: " . $e->getMessage());
         }
     }
 }
-/**
- * Closing connection.
- */
 $conn->close();
     
 
