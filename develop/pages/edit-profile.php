336c13d64f7f5a11cbb89ff87078949a4dca3a78
a <80867545+39hops@users.noreply.github.com>
2024-08-10 20:52:24 -0400

    Added header comments to edit-profile.php

--- develop/pages/edit-profile.php
+++ develop/pages/edit-profile.php
@@ -1,3 +1,9 @@
+<!--Names: Artin Azizi (041131883), Mohamed Dualeh (41137299), Raisa Rahman (041129634)
+    Professor: Alemeseged Legesse
+    File Name: edit-profile.php
+    Date: 8/11/2024
+    Description: PHP file to provide the user the ability to edit their profile.
+-->
 <?php
 
 session_start();
