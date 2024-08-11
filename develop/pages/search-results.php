22ce2dd0286a3354419db4c330b2a61a9053b460
a <80867545+39hops@users.noreply.github.com>
2024-08-11 11:55:59 -0400

    Search-results comments and indentation.

--- develop/pages/search-results.php
+++ develop/pages/search-results.php
@@ -1,29 +1,48 @@
+<!--Names: Artin Azizi (041131883), Mohamed Dualeh (041137299), Raisa Rahman (041129634)
+    Professor: Alemeseged Legesse
+    File Name: edit-profile.php
+    Date: 8/11/2024
+    Description: PHP file to query the correct activities from the their specified search. Will return all activities in a certain city/country in a card format.
+-->
 <?php
+/**
+ * Including database connection script.
+ */
 include("../php/db_connection.php");
-
+/**
+ * Starting session to see if user is logged in.
+ */
 session_start();
 
 if (isset($_SESSION["user"])) {
     $userObj = $_SESSION["user"];
     $userID = $userObj[0]->id;
-
+    /**
+     * Finding the user's itineraries if they have created any.
+     */
     $itinerariesArray = [];
     $sql = "SELECT * FROM itinerary
     WHERE user_id = $userID
     ORDER BY date_created ASC";
 
     $result = $conn->query($sql);
-
+    /**
+     * Fetching results.
+     */
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $itinerariesArray[] = (object) $row;
         }
     }
 }
-
+/**
+ * Retrieving their city and type of activity using GET.
+ */
 $cityID = $_GET['cityID'];
 $activity = $_GET['activity'];
-
+/**
+ * Query to search for all activities with their given parameters.
+ */
 $activitiesArray = [];
 $sql = "SELECT *
 FROM $activity
@@ -34,13 +53,14 @@ ON activity.city_id = city.id
 WHERE city.id = $cityID";
 
 $result = $conn->query($sql);
-
+/**
+ * Fetching query results.
+ */
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $activitiesArray[] = (object) $row;
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -60,7 +80,10 @@ if ($result->num_rows > 0) {
 
 
         <?php
-
+        /**
+         * Conditional navbar, if user is logged in, display their user information as well as links to logout, and edit profile.
+         * If user is not logged in, display links to signup, and login.
+         */
         if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
             echo "<div class='nav'>
             <a href='../pages/index.php'><span id='nomad'>NOMAD</span></a>
@@ -75,13 +98,10 @@ if ($result->num_rows > 0) {
             <a href='../pages/signup.html'>SIGNUP</a>
             </div>";
         }
-
         ?>
-
         <div class='viewing'>
             now viewing <span id='activity'></span> in <span id='city'></span>
         </div>
-
         <div class="modal-bg">
             <div class="glass" id="addModal">
                 <div class="close">&times;</div>
@@ -100,6 +120,9 @@ if ($result->num_rows > 0) {
 </html>
 
 <script>
+    /**
+     * Grabbing all elements.
+     */
     var activitiesArray = <?php echo json_encode($activitiesArray); ?>;
     const container = document.querySelector('.search-results-container');
     const results = document.createElement('div');
@@ -126,69 +149,53 @@ if ($result->num_rows > 0) {
                         echo "0";
                     }
                     ?>;
-
+    // Checking to see if the user name is not null. If not null, display their username.
     if (topName !== null) {
         topName.innerHTML = username;
     }
-
-    console.log(activitiesArray);
-
+    // Inserting information into the corresponding HTML elements.
     activity.innerHTML = <?php echo json_encode($_GET['activity']); ?>;
     city.innerHTML = cityName;
-
     close.addEventListener('click', closeModal);
-
-
+    // Load search function to create a certain number of cards based on how many rows the query returned.
     function loadSearch() {
-
-
         for (let i = 0; i < activitiesArray.length; i++) {
             var card = document.createElement('div');
             var aName = document.createElement('div');
             var aDescription = document.createElement('div');
             var aImage = document.createElement('img');
             var addBtn = document.createElement('div');
-
             aImage.src = activitiesArray[i].image;
             aDescription.innerHTML = activitiesArray[i].a_description;
             aName.innerHTML = activitiesArray[i].a_name;
             addBtn.innerHTML = '+';
             card.dataset.qry = activitiesArray[i].a_id;
-
             card.classList.add('card');
             card.classList.add('glass');
             aName.setAttribute('class', 'activity-name');
             aDescription.setAttribute('class', 'description');
             addBtn.classList.add('add-btn');
             addBtn.classList.add('glass');
-
             card.appendChild(aName);
             card.appendChild(aImage);
             card.appendChild(aDescription);
             card.append(addBtn);
             results.append(card);
             container.append(results);
-
             addBtn.addEventListener('click', openModal);
-
             results.setAttribute('class', 'search-results');
         }
     }
-
+    // Modal load function to allow user to add an activity to their itinerary.
     function loadModal() {
-        console.log(itinerariesArray);
-
         if (itinerariesArray && itinerariesArray.length > 0) {
-
             var select = document.createElement('p');
             select.setAttribute('id', 'message');
             select.innerHTML = 'Select an itinerary: ';
             form.append(select);
-
             for (i = 0; i < itinerariesArray.length; i++) {
                 var field = document.createElement('input');
                 var br = document.createElement('br');
-
                 field.setAttribute('type', 'text');
                 field.setAttribute('name', 'itry');
                 field.readOnly = true;
@@ -198,67 +205,54 @@ if ($result->num_rows > 0) {
                     itineraryID = (e.target).dataset.itineraryId;
                     addModal.dataset.itineraryId = itineraryID;
                 })
-
                 form.append(field);
                 form.append(br);
             }
-
             var addBtn = document.createElement('button');
             addBtn.setAttribute('type', 'button');
             addBtn.innerHTML = 'ADD';
             addBtn.addEventListener('click', addItry);
-
             form.append(addBtn);
-
         } else if (itinerariesArray.length == 0) {
             var select = document.createElement('p');
             var redirect = document.createElement('p');
-
             select.setAttribute('id', 'message');
             redirect.setAttribute('id', 'alternate');
-
             select.innerHTML = 'No itineraries to show';
             redirect.innerHTML = '<a href="../pages/profile.php" id="redirect">GO TO PROFILE</a> to create an itinerary'
-
             form.append(select);
             form.append(redirect);
         } else {
-
             var select = document.createElement('p');
             var redirect = document.createElement('p');
-
             select.setAttribute('id', 'message');
             redirect.setAttribute('id', 'alternate');
-
             select.innerHTML = 'No itineraries to show';
             redirect.innerHTML = '<a href="../pages/login.php" id="redirect">LOGIN</a> to create an itinerary'
             form.append(select);
             form.append(redirect);
         }
     }
-
+    // Function to open modal.
     function openModal(e) {
         modalBG.style.display = "block";
         addModal.style.display = "block";
-
         var activityID = (e.target).parentNode.dataset.qry;
         addModal.dataset.activityId = activityID;
     }
-
+    // Function to close modal.
     function closeModal() {
         modalBG.style.display = "none";
         addModal.style.display = "none";
     }
-
+    // Function to add activity to itinerary.
     function addItry() {
-
         activityID = addModal.dataset.activityId;
         itineraryID = addModal.dataset.itineraryId;
-
         if (!activityID || !itineraryID) {
             console.log('Please an activity to add to your itinerary');
         }
-
+        // Setting information through AJAX to a PHP script that will add the activity to the correct itinerary.
         $.ajax({
             type: "POST",
             url: "../php/add-to-itinerary.php",
@@ -267,18 +261,13 @@ if ($result->num_rows > 0) {
                 itineraryID: itineraryID
             },
             success: function(data) {
-
                 message.style.display = "block";
-
                 setTimeout(function() {
                     location.reload();
                 }, 3000);
-
             }
         });
-
     }
-
     loadSearch();
     loadModal();
 </script>
