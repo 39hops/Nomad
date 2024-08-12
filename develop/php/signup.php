2b637ffd7847784f9f7e01da96be40880a8974d0
mohameddualeh <152139135+mohameddualeh@users.noreply.github.com>
2024-08-11 21:54:07 -0400

    Update signup.php

--- develop/php/signup.php
+++ develop/php/signup.php
@@ -4,31 +4,43 @@
 # File Name: signup.php
 # Date: 8/11/2024
 # Description: PHP file to insert the user's signup information into the database.
+
+# Include database connection file
 include "db_connection.php";
 
-
+# SQL query to insert user information into the user table
 $sql = "INSERT INTO user (first_name, last_name, u_username, email, `u_password`)
 VALUES (?, ?, ?, ?, ?) ";
 
+# Initialize the statement object
 $conn_stmt = $conn->stmt_init();
 
-if(! $conn_stmt->prepare($sql)){
+# Prepare the SQL statement
+if (! $conn_stmt->prepare($sql)) {
     die("SQL Error: " . $conn->error);
-};
+}
+
+# Get the password from POST request and hash it
 $password = $_POST['password'];
 $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+# Bind parameters to the SQL query
 $conn_stmt->bind_param("sssss",
-$_POST["fname"],
-$_POST['lname'],
-$_POST['username'],
-$_POST['email'],
-$passwordHash);
+    $_POST["fname"],
+    $_POST['lname'],
+    $_POST['username'],
+    $_POST['email'],
+    $passwordHash
+);
 
-
- if ($conn_stmt->execute()){
+# Execute the prepared statement
+if ($conn_stmt->execute()) {
+    # Redirect to login page on successful execution
     header("Location: ../pages/login.php");
- } else {
+} else {
+    # Display SQL error if execution fails
     die($conn->error . " ");
- }
+}
 
+# Close the database connection
 $conn->close();
